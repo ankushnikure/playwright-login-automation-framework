db145a437da35f9c6e18a5d73f6eea9f2e639441
ankush <ankush@azafashions.com>
2026-09-21 15:26:30 +0530

    refactor login page methods

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -3,36 +3,27 @@ import { BasePage } from "./base.page";
 
 export class LoginPage extends BasePage {
 
+    // Locators
     private readonly usernameInput: Locator = this.page.getByRole('textbox', { name: 'username' });
     private readonly passwordInput: Locator = this.page.getByRole('textbox', { name: 'password' });
     private readonly submitButton: Locator = this.page.getByRole('button', { name: 'Submit' });
     private readonly loginSuccessMessage: Locator = this.page.getByRole('heading', { name: 'Logged In Successfully' });
     private readonly loginErrorMessage: Locator = this.page.locator('#error');
 
-    async enterUsername(username: string): Promise<void> {
-        await this.usernameInput.fill(username);
-    }
-
-    async enterPassword(password: string): Promise<void> {
-        await this.passwordInput.fill(password);
-    }
-
-    async clickSubmitButton(): Promise<void> {
-        await this.submitButton.click();
-    }
-
-    async login(username: string, password: string): Promise<void> {
-        await this.enterUsername(username);
-        await this.enterPassword(password);
-        await this.clickSubmitButton();
-    }
-
+    // Dynamic locators
     getLoginSuccessMessage(): Locator {
         return this.loginSuccessMessage;
     }
 
     getLoginErrorMessage(): Locator {
         return this.loginErrorMessage;
+    }
+
+    // Methods
+    async login(username: string, password: string): Promise<void> {
+       await this.usernameInput.fill(username);
+       await this.passwordInput.fill(password);
+       await this.submitButton.click();
     }
 
 } 
